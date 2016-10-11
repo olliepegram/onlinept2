5ab65c851669fe2a75d11072d03b072b14c3609f
Ollie Pegram <olliepegram@gmail.com>
2016-10-12 09:30:01 +1100

    fixed database bug

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,7 +3,4 @@ class Listing < ApplicationRecord
   has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-                    :storage => :dropbox,
-                    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-                    :dropbox_options => {...}
 end
