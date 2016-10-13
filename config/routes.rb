a0674106b76b338becb5a754e1d19cfd962c65ef
Ollie Pegram <olliepegram@gmail.com>
2016-10-13 19:07:48 +1100

    Added seller page

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
 
   root 'listings#index'
 
+  get 'seller' => "listings#seller"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
